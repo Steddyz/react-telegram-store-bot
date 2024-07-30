801abfe9f6631bea3fb29f8192ee151c25d15af2
Steddy <qwesteddyoff@gmail.com>
2024-07-30 19:45:25 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import { useEffect } from "react";
 import "./App.css";
 
-const tg = window.Telegram.WebApp;
-
 function App() {
-  const onCLose = () => {
-    tg.close();
-  };
-
   useEffect(() => {
     tg.ready();
   }, []);
 
   return (
     <div className="App">
-      work<button onClick={onCLose}>Закрыть</button>
+      work<button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
