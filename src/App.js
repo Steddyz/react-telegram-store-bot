333db059c10ff0f27d3a2a800daef18930a21898
Steddy <qwesteddyoff@gmail.com>
2024-07-31 10:24:44 +0300

    add redirects

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
       <Routes>
         <Route index element={<ProductList />}></Route>
         <Route path={"/form"} element={<Form />}></Route>
-        <Route path={"*"} element={<ProductList />}></Route>
       </Routes>
     </div>
   );
